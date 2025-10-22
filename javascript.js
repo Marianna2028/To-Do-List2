aa6cdc356dae50962b7dd87cf2534da039bc36cc
Marianna2028 <marianna.regina24@my.stjohns.edu>
2025-10-21 23:47:02 -0400

    Refactor form handling to add new task feature
    Removed old form submission handler and added new task creation functionality.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,16 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('#form').onsubmit = function() {
-    let msg = document.querySelector('#message').value;
-    alert(msg);
-  };
+
+  document.querySelector("#new-task").onsubmit = function() {
+    const li = document.createElement('li');
+    li.innerHTML = document.querySelector('#task').value;
+
+
+    document.querySelector("#tasks_list").append(li);
+    document.querySelector("task").value = '';
+
+    return false;
+    
+    
+}
 });
