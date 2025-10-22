901f4f19db5b82bbf6480f00cdb6f6aa02bd1aa5
Marianna2028 <marianna.regina24@my.stjohns.edu>
2025-10-22 00:13:09 -0400

    Fix typo in task removal function

--- javascript.js
+++ javascript.js
@@ -10,19 +10,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
 //function for removing tasks
     
-        let task_text = docuement.querySelector('#task').value;
+        let task_text = document.querySelector('#task').value;
     let new_task_html = '
       <span> ${task_text} </span> 
-      <button class = "remove"> Completed </button>
+      <button class = "remove"> Remove </button>
     ';
       
       li.innerHTML = new_task_html
-        li.innerHTML = document.querySelector('#task').value; 
-  
     
  document.querySelector("#tasks_list").append(li);
     document.querySelector("#task").value = '';
 
+    //prevents page from regreshing 
     return false;
   
     
