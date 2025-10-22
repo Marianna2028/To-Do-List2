de665153fb61ad285f5c3a334224160b2f898a1a
Marianna2028 <marianna.regina24@my.stjohns.edu>
2025-10-22 00:16:35 -0400

    Refactor task submission logic in javascript.js

--- javascript.js
+++ javascript.js
@@ -1,29 +1,23 @@
-
 document.addEventListener('DOMContentLoaded', function() {
 
-  document.querySelector("#new-task").onsubmit = function() {
-    event.preventDefault();
+  document.querySelector("#new-task").onsubmit = function(event) {
+    event.preventDefault(); // prevents page refresh
 
-    
     const li = document.createElement('li');
-    li.innerHTML = document.querySelector('#task').value;
+    let task_text = document.querySelector('#task').value;
 
-//function for removing tasks
-    
-        let task_text = document.querySelector('#task').value;
-    let new_task_html = '
-      <span> ${task_text} </span> 
-      <button class = "remove"> Remove </button>
-    ';
-      
-      li.innerHTML = new_task_html
-    
- document.querySelector("#tasks_list").append(li);
-    document.querySelector("#task").value = '';
+    let new_task_html = 
+      `
+      <span>${task_text}</span> 
+      <button class="remove">Remove</button>
+    `;
 
-    //prevents page from regreshing 
-    return false;
-  
-    
-}
+    li.innerHTML = new_task_html;
+    document.querySelector("#tasks_list").append(li);
+
+    document.querySelector("#task").value = ''; // clear input
+
+    return false; 
+
 });
+
