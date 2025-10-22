8c46e1c4c44aa72e11d28f00f8b815247de08e93
Marianna2028 <marianna.regina24@my.stjohns.edu>
2025-10-22 00:08:54 -0400

    Implement task creation and removal in javascript.js
    Added functionality to create and remove tasks dynamically.

--- javascript.js
+++ javascript.js
@@ -7,6 +7,19 @@ document.addEventListener('DOMContentLoaded', function() {
     
     const li = document.createElement('li');
     li.innerHTML = document.querySelector('#task').value;
+
+//function for removing tasks
+    
+        let task_text = docuement.querySelector('#task').value;
+    let new_task_html = '
+      <span> ${task_text} </span> 
+      <button class = "remove"> Completed </button>
+    ';
+      
+      li.innerHTML = new_task_html
+        li.innerHTML = document.querySelector('#task').value; 
+  
+    
  document.querySelector("#tasks_list").append(li);
     document.querySelector("#task").value = '';
 
