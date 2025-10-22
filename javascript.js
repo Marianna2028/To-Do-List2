4a524bdb7d18f1742f5773e5044d74ed1d53a613
Marianna2028 <marianna.regina24@my.stjohns.edu>
2025-10-21 23:51:08 -0400

    Fix task submission logic in javascript.js

--- javascript.js
+++ javascript.js
@@ -2,12 +2,11 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   document.querySelector("#new-task").onsubmit = function() {
+    
     const li = document.createElement('li');
     li.innerHTML = document.querySelector('#task').value;
-
-
-    document.querySelector("#tasks_list").append(li);
-    document.querySelector("task").value = '';
+ document.querySelector("#tasks_list").append(li);
+    document.querySelector("#task").value = '';
 
     return false;
     
